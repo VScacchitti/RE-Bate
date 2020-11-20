84a48f8c35c6465331012bea8b4fa7d07f17b8fb
Vince Scacchitti <VScacchitti24@gmail.com>
2020-11-20 16:10:02 -0500

    added and cleaned up comments.

--- routes/api-routes-comment.js
+++ routes/api-routes-comment.js
@@ -6,7 +6,6 @@ const db = require("../models");
 // =============================================================
 module.exports = function(app) {
   // GET route for getting all of the posts
-  // eslint-disable-next-line prettier/prettier
   app.get("/api/comments", (req, res) => db.Comment.findAll().then(result => res.json(result))
   );
 
@@ -19,29 +18,26 @@ module.exports = function(app) {
   app.get("/api/comments/:content", (req, res) => db.Comment.findAll().then(result => res.json(result))
   );
 
-  // Get route for retrieving a single post
+  // Get route for retrieving a single comment
   app.get("/api/comments/:id", (req, res) => {
-    // Here we add an "include" property to our options in our findOne query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
+   
     db.Comment.findOne({
       where: {
         id: req.params.id
       },
-      include: [db.Author]
     }).then(dbComment => {
       res.json(dbComment);
     });
   });
 
-  // POST route for saving a new post
+  // POST route for saving a new comment
   app.post("/api/comments", (req, res) => {
     db.Comment.create(req.body).then(dbComment => {
       res.json(dbComment);
     });
   });
 
-  // DELETE route for deleting posts
+  // DELETE route for deleting comment
   app.delete("/api/comments/:id", (req, res) => {
     db.Comment.destroy({
       where: {
@@ -52,7 +48,6 @@ module.exports = function(app) {
     });
   });
 
-  // PUT route for updating posts
   app.put("/api/commments", (req, res) => {
     db.Comment.update(req.content, {
       where: {
--- routes/api-routes-topics.js
+++ routes/api-routes-topics.js
@@ -2,18 +2,12 @@ const db = require("../models");
 
 module.exports = function(app) {
   app.get("/api/topics", (req, res) => {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
     db.Topic.findAll({}).then(dbTopic => {
       res.json(dbTopic);
     });
   });
 
   app.get("/api/topics/:id", (req, res) => {
-    // Here we add an "include" property to our options in our findOne query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
     db.Topic.findOne({
       where: {
         id: req.params.id
